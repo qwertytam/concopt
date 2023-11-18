2973348af302bf8f4d95a9b8b16f4ad929702140
qwertytam <tom.q.marshall@gmail.com>
2023-11-17 19:31:54 -0500

    Simplify, add pressure and speed conversions

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,19 @@
-from pint import UnitRegistry    
+from constants import T_ice
+from constants import mins_per_deg
+from constants import kts_per_ms
 
 def convert_dms_to_dd(tude):
     multiplier = 1 if tude[-1] in ['N', 'E'] else -1
-    dd = sum(float(x) / 60 ** n for n, x in enumerate(tude[:-1].split('-')))
+    dd = sum(
+        float(x) / mins_per_deg ** n for n, x in enumerate(
+            tude[:-1].split('-')))
     return multiplier * dd
 
 def convert_degc_to_degK(degC):
-    ureg = UnitRegistry()
-    Q_ = ureg.Quantity
-    temp = Q_(degC, ureg.degC)
-    return temp.to('degK').magnitude
+    return degC + T_ice
+
+def convert_hpa_to_pa(hpa):
+    return hpa * 100
+
+def convert_kts_to_ms(kts):
+    return kts * kts_per_ms
