72177746d2c4246b6df3915d533e0b1b50ea3fd9
qwertytam <tom.q.marshall@gmail.com>
2023-11-21 17:00:46 -0500

    Simplify, use pint for conversions and fc for constants

--- src/constants.py
+++ src/constants.py
@@ -1,16 +1,11 @@
-from pint import UnitRegistry
 from flightcondition.constants import PhysicalConstants as physicals
 from flightcondition.constants import AtmosphereConstants as atmosphericals
+from flightcondition import FlightCondition, unit
 
-ureg = UnitRegistry()
-kts_per_ms = 1 * ureg.knots
-kts_per_ms = kts_per_ms.to('m/s').magnitude
+mins_per_deg = 60
 
-R_star = physicals.R_air.magnitude
 gamma = physicals.gamma_air.magnitude
 
 p_0 = atmosphericals.p_0.magnitude
-T_ice = atmosphericals.T_ice.magnitude
-rho_0 = atmosphericals.rho_0.magnitude
 
-mins_per_deg = 60
+a_0 = FlightCondition(h=0*unit('km'), units='SI').a
