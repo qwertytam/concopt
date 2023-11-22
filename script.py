ddb522e908db5c5599dc1ebcd24d2d1745c69b2a
qwertytam <tom.q.marshall@gmail.com>
2023-11-22 13:57:18 -0500

    working version

--- script.py
+++ script.py
@@ -1,24 +1,32 @@
 import logging
-from pint import UnitRegistry
+import numpy as np
+import pandas as pd
+import pint
 import pint_pandas
+import matplotlib.pyplot as plt
 from flightcondition import FlightCondition, unit
 from src.conditions import calc_gs_for_atmosphere, get_sound_speed
 from src.conditions import calc_mach_from_cas, calc_tas_from_mach, stagnation_temp
 from src.conditions import mach_from_temps
 from src.asky import get_atmosphere_as_pd
-def main():
-    ureg = UnitRegistry()
-    Q_ = ureg.Quantity
+from data.conc_data import get_cas_limit, get_mach_limit, get_total_temp_limit
 
-    # lat = 29.6
-    # lon = -98.33
+def main():
+    # ureg = pint.UnitRegistry()
+    # Q_ = ureg.Quantity
+
+    # --------------------------------
+    # Get weather for given position
     lat = '10-57-21.24S'
     lon = '115-51-38.16E'
     tude_unts = 'dms'
 
-    alts = list(range(50*10**3, 60*10**3 + 1, 1*10**3))
+    # alts = list(range(50*10**3, 60*10**3 + 1, 1*10**3))
+    alts = list(range(40*10**3, 60*10**3 + 1, 1*10**3))
     atmos = get_atmosphere_as_pd(lat, lon, alts, tude_units=tude_unts)
 
+    # --------------------------------
+    # Get ambient speed of sound
     atmos.insert(loc=len(atmos.columns), column='SpeedSound', value=None)
     for idx, row in atmos.iterrows():
         ss = get_sound_speed(
@@ -28,42 +36,46 @@ def main():
         atmos.iloc[idx, len(atmos.columns)-1] = ss.magnitude
 
     atmos.SpeedSound = pint_pandas.PintArray(atmos.SpeedSound, dtype=ss_units)
+
+    # --------------------------------
+    # Get airframe cas, mach and temp limits
+    atmos['CASLimit'] = pint_pandas.PintArray(
+        get_cas_limit(atmos.Altitude), dtype='knots')
+    atmos['MachLimit'] = get_mach_limit(atmos.Altitude)
+    atmos['TotalTempLimit'] = get_total_temp_limit(atmos.Altitude)
+    atmos.TotalTempLimit = pint_pandas.PintArray(
+        atmos.TotalTempLimit, dtype='degC')
+
+    # --------------------------------
+    # Get max mach for each of cas and temp limits
+    atmos.insert(loc=len(atmos.columns), column='MachFromCASLimit', value=None)
+    for idx, row in atmos.iterrows():
+        mach = calc_mach_from_cas(
+            cas=row.CASLimit.to('m/s').magnitude,
+            altitude=row.Altitude,
+            static_temp=row.Temperature.to('degC').magnitude)
+        atmos.iloc[idx, len(atmos.columns)-1] = mach.magnitude
+
+    atmos.MachFromCASLimit = atmos.MachFromCASLimit.astype('float64')
+
+    atmos['MachfromTotalTempLimit'] = mach_from_temps(atmos.TotalTempLimit,
+                                                     atmos.Temperature)
+
+    # --------------------------------
+    # Get tas limit and gs
+    atmos['MaxMach'] = atmos.loc[:, ['MachLimit', 'MachFromCASLimit', 'MachfromTotalTempLimit']].min(axis=1)
+    atmos['MaxTAS'] = atmos.SpeedSound * atmos.MaxMach
+    atmos['GS'] = calc_gs_for_atmosphere(atmos, atmos.MaxTAS, 100)
+
+    # --------------------------------
+    # Find maximum gs and altitude
+    max_gs_row = atmos.GS.argmax()
+    print(atmos.iloc[max_gs_row, :])
     
-    mach = 2.04
-    heading = 100
-    atmos = calc_gs_for_atmosphere(atmos, mach, heading)    
-    print(atmos)
-    print(atmos.dtypes)
-
-
-    alt = 50189*unit('ft')
-    fc = FlightCondition(h=alt, units='SI')
-    fc = FlightCondition(h=alt.magnitude/fc.H.to('ft').magnitude * alt, units='SI') 
-    isa = +10*unit('delta_degC')
-    fc.T = fc.T + isa
-    Vc = 530*unit('knots')
-
-    mach = calc_mach_from_cas(Vc, altitude=alt, isa_temp_diff=isa)
-    tas = calc_tas_from_mach(mach, altitude=alt, isa_temp_diff=isa)
-    total_temp = stagnation_temp(fc.T, mach)
-    print(f"Result out:\n" +
-          f"FL{(fc.h.to('ft').magnitude/100):.0f}  {fc.h.to('ft'):~.0f}\n" +
-          f"CAS {Vc.to('knots'):~.0f}\n" +
-          f"OAT {fc.T.to('degK'):~.1f}   {fc.T.to('degC'):~.1f}  " +
-          f"ISA {(fc.T - isa).to('delta_degC'):~.1f}\n" +
-          f"Local M1 {fc.a.to('knots'):~.0f}\n" +
-          f"Mach {mach:~.3f}\n" +
-          f"TAS {tas.to('knots'):~.0f}\n" +
-          f"Static temp {fc.T.to('degC'):~.1f} " +
-          f"Total temp {total_temp.to('degC'):~.1f}")
-
-
-    max_temp = Q_(127, ureg.degC)
-    static_temp = [-65, -56.5, -45]
-    
-    for st in static_temp:
-        max_mach = mach_from_temps(max_temp, Q_(st, ureg.degC))
-        print(f"Temp limtied mach: {max_mach:~.3f}")
+    plt.plot(atmos.GS.pint.to('knots').values.quantity.m,
+             atmos.Altitude.pint.to('ft').values.quantity.m,
+             marker='o', color='k')
+    plt.show()    
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.WARN)
