57b879571e17d8e630c265e72d5f80d71d5ee9ee
qwertytam <tom.q.marshall@gmail.com>
2023-11-28 10:09:33 -0500

    Add wrapper

--- src/concopt/units.py
+++ src/concopt/units.py
@@ -6,7 +6,7 @@ Author: Tom Marshall
 :copyright: 2023 Tom Marshall
 :license: MIT License, see LICENSE for more details.
 """
-
+from functools import wraps
 from pint import UnitRegistry
 import pint_pandas
 
@@ -17,3 +17,20 @@ dimless = unit('dimensionless')
 
 pint_pandas.PintType.ureg = unit
 ppunit = pint_pandas.PintType.ureg
+
+def to_base_units_wrapper(func):
+    """Function decorator to convert output variable units to base units.
+
+    Args:
+        func (callable): Function to wrap
+
+    Returns:
+        callable: Called, wrapped function
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        output = func(*args, **kwargs)
+        if output is not None:
+            output.ito_base_units()
+        return output
+    return wrapper
