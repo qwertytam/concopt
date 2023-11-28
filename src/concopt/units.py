40c3719ba227dea1d2be98de53cd78228e03e546
qwertytam <tom.q.marshall@gmail.com>
2023-11-27 20:45:26 -0500

    Setting own registry

--- src/concopt/units.py
+++ src/concopt/units.py
@@ -1,6 +1,10 @@
 from pint import UnitRegistry
+import pint_pandas
 
 unit = UnitRegistry(system='mks')
 
 unit.default_format = '~P'
 dimless = unit('dimensionless')
+
+pint_pandas.PintType.ureg = unit
+ppunit = pint_pandas.PintType.ureg
