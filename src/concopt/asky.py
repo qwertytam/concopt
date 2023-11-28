ae2853bd4bae59e9a6b2c629ad64c17a23a89f11
qwertytam <tom.q.marshall@gmail.com>
2023-11-27 20:45:07 -0500

    Response check and using own pint registry

--- src/concopt/asky.py
+++ src/concopt/asky.py
@@ -1,9 +1,10 @@
 import requests
 import logging
 import pandas as pd
-from pint import UnitRegistry
 import pint_pandas
-from .utils import convert_dms_to_dd
+
+from concopt.utils import convert_dms_to_dd
+from concopt.units import ppunit
 
 # defaults
 url_base = 'http://'
@@ -15,7 +16,7 @@ def get_atmosphere(lat, lon, alts,
     Arguments:
         lat -- latitude for given position
         lon -- longitude for given position
-        alts -- list of altitudes to get conditions for
+        alts -- list of altitudes to get conditions for in feet
         host_addr -- ActiveSky host address, 'localhost' by default
         port -- ActiveSky port, 19285 by default
         tude_units -- units for lat and lon; decimal or 'dd' by default; can
@@ -38,7 +39,7 @@ def get_atmosphere(lat, lon, alts,
     end_point = '/ActiveSky/API/GetAtmosphere?'
     latr = f"lat={lat}"
     lonr = f"&lon={lon}"
-    alts = [str(x) for x in alts] # confirm elements are string before concat
+    alts = [str(int(x)) for x in alts] # confirm elements are string  and integers before concat
     altsr = f"&altitudes={"|".join(alts)}"
 
     req = f"{url_base}{host_addr}:{port}{end_point}{latr}{lonr}{altsr}"
@@ -48,7 +49,12 @@ def get_atmosphere(lat, lon, alts,
     logging.info(
         f"Received response with status code {r.status_code}\n{r.text}")
     
-    return r.json()
+    if r.content == "Error":
+        json = None
+    else:
+        json = r.json()
+
+    return json
 
 
 def get_atmosphere_as_pd(lat, lon, alts,
@@ -69,16 +75,15 @@ def get_atmosphere_as_pd(lat, lon, alts,
         atmosphere in json format; see ActiveSky API documentation for further
         detail
     """
-    atmos = get_atmosphere(lat, lon, alts, tude_units=tude_units)
+    atmos = get_atmosphere(lat, lon,
+                           alts.to('ft').magnitude,
+                           tude_units=tude_units)
     atmos = pd.DataFrame.from_dict(atmos['WeatherData'], dtype='Float32')
-
-    ureg = UnitRegistry()
-    # pint_pandas.PintType.ureg = ureg
 
     atmos.Altitude = pint_pandas.PintArray(atmos.Altitude, dtype="feet")
     atmos.WindDirection = pint_pandas.PintArray(atmos.WindDirection,dtype="degrees")
     atmos.WindSpeed = pint_pandas.PintArray(atmos.WindSpeed, dtype="knots")
     atmos.Pressure = pint_pandas.PintArray(atmos.Pressure, dtype="hPa")
-    atmos.Temperature = pint_pandas.PintArray(atmos.Temperature, ureg.Unit('degC'))
+    atmos.Temperature = pint_pandas.PintArray(atmos.Temperature, ppunit.Unit('degC'))
 
     return atmos
